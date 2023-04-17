f6175f67a33a2ead5295b60ec14d2ce58edddbf4
Gaston <gaston.molinaf90@gmail.com>
2023-04-17 15:45:34 -0300

    ajax y form
    Aplicacion de Ajax y reconstruccion de formulario

---
+++ ajax.js
@@ -0,0 +1,23 @@
+function GuardarDatos() {
+
+    let nom=$("#nombre").val();
+    let ape=$("#apellido").val();
+    let em=$("#email").val();
+    let tel=$("#tel").val();
+    let cons=$("#consulta").val();
+
+    $.ajax({
+        url: "https://reqres.in/api/users",
+        type: "POST",
+        data: {
+           nombre: nom,
+           apellido: ape,
+           email: em,
+           telefono: tel,
+           consulta: cons,
+        },
+        success: function(response){
+            console.log(response);
+        }
+    });
+}
