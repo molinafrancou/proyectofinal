d64d52052299871996711eb6ff0127da9793860b
Gaston <gaston.molinaf90@gmail.com>
2023-04-19 15:19:35 -0300

    Final
    Trabajo finalizado, falta consultar por que no me toma el json de las consulta.

--- ajax.js
+++ ajax.js
@@ -22,3 +22,29 @@ function GuardarDatos() {
     });
 }
 
+document.querySelector('#dolar').addEventListener('click', obtenerDatos)
+
+function obtenerDatos()
+{
+  let url='https://www.dolarsi.com/api/api.php?type=valoresprincipales';  
+  const api= new XMLHttpRequest();
+    api.open('GET',url,true);
+    api.send();
+    api.onreadystatechange = function(){
+          if(this.readyState==4 && this.status==200)
+          {
+            
+            let datos=JSON.parse(this.responseText);
+            console.log(datos);
+            let resultado=document.querySelector('#resultado');
+            resultado.innerHTML='';
+                       
+            for(let item of datos)
+            {
+                            
+                resultado.innerHTML+='<li>'+item.casa.nombre+' - '+item.casa.compra+' - '+item.casa.venta+'</li>';
+                              
+            } 
+          }
+    }
+}
